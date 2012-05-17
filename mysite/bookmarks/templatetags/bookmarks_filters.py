4dcacc6676c9370a41840dc04c609f3458fc48b3
Nguyen Van Hoang <hoangnv_55@vnu.edu.vn>
2012-05-18 01:05:16 +0700

    don dep

--- mysite/bookmarks/templatetags/bookmarks_filters.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def capitalize(value):
-	return value.capitalize()
--- mysite/site_media/tag_autocomplete.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-  $("#id_tags").autocomplete(
-     '/ajax/tag/autocomplete/',
-     {multiple: true, multipleSeparator: ' '}
-  );
-});
