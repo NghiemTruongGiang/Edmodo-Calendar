be460359b4cb1ee70a9987256ab02e6d92d4bced
Nguyen Van Hoang <hoangnv_55@vnu.edu.vn>
2012-05-24 17:06:59 +0700

    test

--- Calendar_Learn/Calendar/tests.py
+++ Calendar_Learn/Calendar/tests.py
@@ -1,16 +1,57 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class CalendarTest(TestCase):
+    def test_register_page(self):
+        data={
+            'username':'admin',
+            'email':'email@gmail.com',
+            'password1':'123456',
+            'password2':'123456',
+        }
+        response = self.client.post('/register/',data)
+        self.assertContains(response,"Register")
+    def test_login(self):
+        response = self.client.post('/accounts/login/',{
+            'usename' : 'admin',
+            'password' : 'admin',
+        })
+        self.assertEqual(response.status_code, 200)
+	def test_create_group(self):
+        response = self.client.post('/accounts/login/',{
+            'usename' : 'admin',
+            'password' : 'admin',
+            })
+        self.assertEqual(response.status_code, 200)
+        self.client.post('/create/group/',{
+			'name' : 'Group name',
+			'describe' : 'Group description',
+			'group_email' : 'Group email address',
+			})
+		self.assertEqual(response.status_code, 200)
+	def test_change_name_form(self):
+		response = self.client.post('/accounts/login/',{
+            'usename' : 'admin',
+            'password' : 'admin',
+            })
+		self.assertEqual(response.status_code, 200)
+		self.client.post('/change/form/',{
+            'first_name' : 'First Name',
+			'last_name' : 'Last Name',
+			'email' : 'Email',
+			'birthday' : 'Birthday',
+            })
+		self.assertEqual(response.status_code, 200)
+	def test_password_change_form(self):
+		response = self.client.post('/accounts/login/',{
+            'usename' : 'admin',
+            'password' : 'admin',
+            })
+		self.assertEqual(response.status_code, 200)
+		self.client.post('/change/password/',{
+			'old_password' : 'Old Password',
+			'new_password1' : 'New Password',
+			'new_password2' : 'Confirm New Password',
+			})
+		self.assertEqual(response.status_code, 200)
+		
